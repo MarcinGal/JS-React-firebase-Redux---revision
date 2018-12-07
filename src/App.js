94aa491eec0f724bb0d329e0d0dc196a85573514
marcingal <marcin921125@gmail.com>
2018-12-07 11:51:14 +0100

    added link to paths

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from 'react'
 import Counter from './Counter'
 import PassingProps from './PassingProps'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
       <Router>
+        <div>
+        <Link to='/'>
           <div>
-            <Route path="/" exact component={Counter} />
-            <Route path="/counter" component={Counter} />
-            <Route path="/passing-props" component={() =>
-              <PassingProps
-                passingValue={5}
-                passingFun={() => alert('bu')}
-              />}
-            />
+            Home
           </div>
+        </Link>
+        <Link to='/counter'>
+          <div>
+            Counter
+          </div>
+        </Link>
+        <Link to='/passing-props'>
+          <div>
+            Passing Props
+          </div>
+        </Link>
+        <Link to='/counter-with-start-value'>
+          <div>
+            Counter with start value
+          </div>
+        </Link>
+        <div>
+          <Route path="/" exact component={Counter} />
+          <Route path="/counter" component={Counter} />
+          <Route path="/passing-props" component={() =>
+            <PassingProps
+              passingValue={5}
+              passingFun={() => alert('bu')}
+            />
+          } />
+          <Route path="/counter-with-start-value" component={() =>
+            <Counter
+              startValue={15}
+            />
+          } />
+        </div>
+        </div>
       </Router>
     )
   }
