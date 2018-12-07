dfc624f550035fae7986c3953b4dac11f6d3b0ac
marcingal <marcin921125@gmail.com>
2018-12-07 11:40:36 +0100

    router added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Counter from './Counter'
 import PassingProps from './PassingProps'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Counter />
-        <PassingProps
-          passingValue={5}
-          passingFun={() => alert('bu')}
-        />
-      </div>
+      <Router>
+          <div>
+            <Route path="/" exact component={Counter} />
+            <Route path="/counter" component={Counter} />
+            <Route path="/passing-props" component={() =>
+              <PassingProps
+                passingValue={5}
+                passingFun={() => alert('bu')}
+              />}
+            />
+          </div>
+      </Router>
     )
   }
 }
