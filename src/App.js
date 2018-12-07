623a508e2fec60a1a7fadb17a58d1012e3563f7e
marcingal <marcin921125@gmail.com>
2018-12-07 12:30:38 +0100

    navigation completed

--- src/App.js
+++ src/App.js
@@ -2,51 +2,56 @@ import React from 'react'
 import Counter from './Counter'
 import PassingProps from './PassingProps'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { ListItem } from 'material-ui/List'
+import Navigation from './Navigation'
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div>
-        <Link to='/'>
+          <Navigation>
+            <Link to='/'>
+              <ListItem>
+                Home
+            </ListItem>
+            </Link>
+            <Link to='/counter'>
+              <ListItem>
+                Counter
+            </ListItem>
+            </Link>
+            <Link to='/passing-props'>
+              <ListItem>
+                Passing Props
+            </ListItem>
+            </Link>
+            <Link to='/counter-with-start-value'>
+              <ListItem>
+                Counter with start value
+            </ListItem>
+            </Link>
+          </Navigation>
+
           <div>
-            Home
+            <Route path="/" exact component={Counter} />
+            <Route path="/counter" component={Counter} />
+            <Route path="/passing-props" component={() =>
+              <PassingProps
+                passingValue={5}
+                passingFun={() => alert('bu')}
+              />
+            } />
+            <Route path="/counter-with-start-value" component={() =>
+              <Counter
+                startValue={15}
+              />
+            } />
           </div>
-        </Link>
-        <Link to='/counter'>
-          <div>
-            Counter
-          </div>
-        </Link>
-        <Link to='/passing-props'>
-          <div>
-            Passing Props
-          </div>
-        </Link>
-        <Link to='/counter-with-start-value'>
-          <div>
-            Counter with start value
-          </div>
-        </Link>
-        <div>
-          <Route path="/" exact component={Counter} />
-          <Route path="/counter" component={Counter} />
-          <Route path="/passing-props" component={() =>
-            <PassingProps
-              passingValue={5}
-              passingFun={() => alert('bu')}
-            />
-          } />
-          <Route path="/counter-with-start-value" component={() =>
-            <Counter
-              startValue={15}
-            />
-          } />
-        </div>
         </div>
       </Router>
     )
   }
 }
 
-export default App;
+export default App
---
+++ src/Navigation.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import Drawer from 'material-ui/Drawer'
+import { AppBar } from 'material-ui'
+import { Link } from 'react-router-dom'
+import { ListItem } from 'material-ui/List'
+
+class Navigation extends React.Component {
+    state = {
+        isDrawerOpen: true
+    }
+
+
+    toggleDrawer = () => this.setState({ isDrawerOpen: !this.state.isDrawerOpen })
+
+
+    render() {
+        // can be better optimized
+        const MenuLink = (props) => (
+          <Link
+            to={props.to}
+            style={{ textDecoration: 'none' }}
+            onClick={this.toggleDrawer}
+          >
+            <ListItem>
+              {props.label}
+            </ListItem>
+          </Link>
+        )
+    
+        return (
+          <div>
+            <AppBar
+              onLeftIconButtonClick={this.toggleDrawer}
+            />
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.isDrawerOpen}
+              onRequestChange={this.toggleDrawer}
+            >
+              <MenuLink to={'/'} label="Home" />
+              <MenuLink to={'/counter'} label="Counter" />
+              <MenuLink to={'/counter-with-start-value'} label="Counter 15" />
+              <MenuLink to={'/passing-props'} label="Passing Props" />
+            </Drawer>
+          </div>
+        )
+      }
+    }
+    
+
+    export default Navigation
